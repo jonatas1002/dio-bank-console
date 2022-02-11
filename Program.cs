ee9e1262aa526641b2251393ba438c2f75f8868a
Jonatas <jonatas1002@gmail.com>
2022-02-11 15:26:22 -0400

    Versão 1.0 do sistema DIO Bank console

--- Program.cs
+++ Program.cs
@@ -1,26 +1,135 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 1000, 500, "Jonatas Stein Ribeiro II");
-            Conta contaDois = new Conta(TipoConta.PessoaFisica, 500, 500, "Jonatas Stein");
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine(minhaConta.ToString());
+            while(opcaoUsuario.ToUpper() != "X"){
+                switch(opcaoUsuario){
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        Transferir();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços!");
+            Console.ReadLine();
+        }
 
-            minhaConta.Depositar(500);
+        private static void Transferir()
+        {
+            Console.Write("Digite o número da conta para saque: ");
+            int indiceContaSaque = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(minhaConta.ToString());
+            Console.Write("Digite o valor a ser transferido: ");
+            double valorTransferencia = double.Parse(Console.ReadLine());
 
-            minhaConta.Sacar(900);
-            Console.WriteLine(minhaConta.ToString());
-            minhaConta.Transferir(700, contaDois);
-            minhaConta.Sacar(300);
-            Console.WriteLine(minhaConta.ToString());
-            Console.WriteLine(contaDois.ToString());
+            Console.Write("Digite o número da conta para deposito: ");
+            int indiceContaDeposito = int.Parse(Console.ReadLine());            
+
+            listContas[indiceContaSaque].Transferir(valorTransferencia, listContas[indiceContaDeposito]);
+        }
+
+        private static void Depositar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Depositar(valorDeposito);
+        }
+
+        private static void Sacar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorSaque);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas:");
+            if(listContas.Count == 0){
+                Console.WriteLine("Nenhuma conta cadastrada.");
+                return;
+            }
+            for(int i=0; i < listContas.Count; i++){
+                Conta conta = listContas[i];
+                Console.Write("#{0} - ", i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+            
+            Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+
+            listContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine();
+            Console.WriteLine("Seja bem vindo à DIO Bank!");
+            Console.WriteLine("Informe a opçao desejada:");
+
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transfereir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
